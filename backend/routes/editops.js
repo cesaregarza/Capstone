e369569e208584ae5c6ed756111c660ad0577559
Unknown <cesar.garza.826@gmail.com>
2018-08-23 16:12:32 -0500

    Options 40% done

--- backend/routes/editops.js
+++ backend/routes/editops.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const mongoost = require("mongoose");
+const mongoose = require("mongoose");
 const multer = require("multer");
 const scrypt = require("scrypt");
 const checkAuth = require("../middleware/check-auth");
