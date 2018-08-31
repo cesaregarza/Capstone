5884f8e0c11d4a2f231840df26576b92e5a8c33f
Jaime Meza <hello@jaimemeza.com>
2018-08-30 19:26:46 -0500

    Create center 95% and link from signup page

--- frontend/src/app/center-register/center-register.component.ts
+++ frontend/src/app/center-register/center-register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ChangeDetectorRef, ViewChild } from "@angular/core";
-import * as $ from 'jquery';
+import * as $ from "jquery";
 import {
   FormControl,
   FormGroupDirective,
@@ -13,7 +13,6 @@ import { NavbarComponent } from "../navbar/navbar.component";
 import { HttpClient } from "@angular/common/http";
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-
   isErrorState(
     control: FormControl | null,
     form: FormGroupDirective | NgForm | null
@@ -28,12 +27,15 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 }
 
 @Component({
-  selector: 'app-center-register',
-  templateUrl: './center-register.component.html',
-  styleUrls: ['./center-register.component.scss']
+  selector: "app-center-register",
+  templateUrl: "./center-register.component.html",
+  styleUrls: ["./center-register.component.scss"]
 })
 export class CenterRegisterComponent implements OnInit {
-  @ViewChild("fileInput") fileInput;
+  @ViewChild(FormGroupDirective)
+  formGroupDirective: FormGroupDirective;
+  @ViewChild("fileInput")
+  fileInput;
   selectedFile = null;
   selectedFileName = "";
   enviroment = {
@@ -51,21 +53,28 @@ export class CenterRegisterComponent implements OnInit {
   ngOnInit() {}
 
   form = new FormGroup({
-    name: new FormControl("", [Validators.required]),
+    name: new FormControl("", [Validators.required, Validators.maxLength(254)]),
 
-    address: new FormControl("", [Validators.required]),
+    address: new FormControl("", [Validators.required, Validators.maxLength(254)]),
 
-    location: new FormControl("", [Validators.required]),
+    location: new FormControl("", [Validators.required, Validators.maxLength(254)]),
 
-    postal: new FormControl("", [Validators.required]),
+    postal: new FormControl("", [Validators.required, Validators.max(99999)]),
 
-    phone: new FormControl("", [Validators.required]),
+    phone: new FormControl("", [Validators.required, Validators.maxLength(20)]),
 
-    email: new FormControl("", [Validators.required]),
+    email: new FormControl("", [
+      Validators.required,
+      Validators.email,
+      Validators.maxLength(254),
+      Validators.pattern(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+    ]),
 
-    hours: new FormControl("", [Validators.required]),
+    hours: new FormControl("", [Validators.required, Validators.maxLength(254)]),
 
-    tokenId: new FormControl("", [Validators.required]),
+    tokenId: new FormControl("", [Validators.required, Validators.maxLength(24)]),
 
     picture: new FormControl("", [Validators.required]),
     passwords: new FormGroup(
@@ -90,22 +99,21 @@ export class CenterRegisterComponent implements OnInit {
     let password = g.get("password").errors;
     let passwordConfirm = g.get("passwordConfirm").errors;
     if (g.get("password").value === g.get("passwordConfirm").value) {
-
       if (password !== null) {
         password.mismatch != undefined ? delete password.mismatch : "";
       }
 
       if (passwordConfirm !== null) {
-        passwordConfirm.mismatch != undefined ? delete passwordConfirm.mismatch : "";
+        passwordConfirm.mismatch != undefined
+          ? delete passwordConfirm.mismatch
+          : "";
       }
 
-      if ($.isEmptyObject(password))
-      {
-          password = null;
+      if ($.isEmptyObject(password)) {
+        password = null;
       }
-      if ($.isEmptyObject(passwordConfirm))
-      {
-          passwordConfirm = null;
+      if ($.isEmptyObject(passwordConfirm)) {
+        passwordConfirm = null;
       }
 
       g.get("password").setErrors(password);
@@ -126,17 +134,22 @@ export class CenterRegisterComponent implements OnInit {
 
   onFileChange = event => {
     let image: File = event.target.files[0];
-    if (image.size < 2097100 && (image.type == 'image/jpeg' || image.type == 'image/png')) {
-  this.selectedFile = event.target.files[0];
-  this.selectedFileName = this.selectedFile.name;
+    if (
+      image.size < 2097100 &&
+      (image.type == "image/jpeg" || image.type == "image/png")
+    ) {
+      this.selectedFile = event.target.files[0];
+      this.selectedFileName = this.selectedFile.name;
     } else {
-      this.form.get('picture').setErrors({type: 'error'})
+      this.form.get("picture").setErrors({ type: "error" });
     }
   };
 
-  sendForm = (values) =>  {
-    console.log(values.passwords)
-    let cURL = values.name.toLowerCase().split(' ').join('')
+  sendForm = values => {
+    let cURL = values.name
+      .toLowerCase()
+      .split(" ")
+      .join("");
     let tempForm = new FormData();
     tempForm.append("name", values.name);
     tempForm.append("address", values.address);
@@ -145,33 +158,39 @@ export class CenterRegisterComponent implements OnInit {
     tempForm.append("phone", values.phone);
     tempForm.append("email", values.email);
     tempForm.append("hours", values.hours);
-    tempForm.append("picture", this.selectedFile, cURL + this.logic.pictureId() + ".png");
+    tempForm.append(
+      "picture",
+      this.selectedFile,
+      cURL + this.logic.pictureId() + ".png"
+    );
     tempForm.append("cURL", cURL);
-    tempForm.append('isDeleted', 'false');
-    tempForm.append('tokenId', values.tokenId);
-    tempForm.append('date_joined', Date.now().toString());
-    tempForm.append('last_login', Date.now().toString());
-    tempForm.append('usertype', '2');
-    tempForm.append('password', values.passwords.password);
+    tempForm.append("isDeleted", "false");
+    tempForm.append("tokenId", values.tokenId);
+    tempForm.append("date_joined", Date.now().toString());
+    tempForm.append("last_login", Date.now().toString());
+    tempForm.append("usertype", "2");
+    tempForm.append("password", values.passwords.password);
 
     // let tempForm = new Form
     if (this.form.status === "VALID") {
       this.http.post(this.enviroment.url, tempForm).subscribe(
         result => {
-          if (result['status'] == 201) {
-         this.form.reset();
-         this.selectedFile = null;
-         this.selectedFileName = "";
+          if (result["status"] == 201) {
+            this.formGroupDirective.resetForm();
+            this.selectedFile = null;
+            this.selectedFileName = "";
           }
         },
         err => {
-          err['status'] == 409 ? this.form.controls.email.setErrors({already : true}) : '';
-          err['status'] == 401 ? this.form.controls.tokenId.setErrors({required : true}) : '';
+          err["status"] == 409
+            ? this.form.controls.email.setErrors({ already: true })
+            : "";
+          err["status"] == 401
+            ? this.form.controls.tokenId.setErrors({ required: true })
+            : "";
         }
       );
-
     }
   };
   matcher = new MyErrorStateMatcher();
 }
-
